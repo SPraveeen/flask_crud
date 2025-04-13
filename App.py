c6be0611ce40e0be057361cd3bd635917650c4f8
SPraveeen <praveenji8498@gmail.com>
2025-04-13 20:24:51 +0530

    adding

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ db.init_app(app)
 # @app.before_first_request
 def create_table():
     db.create_all()
-    
+
 with app.app_context():
     create_table()
 
@@ -17,5 +17,82 @@ with app.app_context():
 def create():
     if request.method == 'GET':
         return render_template('create.html')
+    
+    if request.method == 'POST':
+        hobby = request.form.getlist('hobbies')
+        hobbies = ",".join(map(str,hobby))
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form['gender']
+        country = request.form['country']
+        
+        students =StudentModel(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            gender=gender,
+            hobbies=hobbies,
+            country=country
+        )
+        db.session.add(students)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/create',methods=['GET'])
+def RetrieveList():
+    students=StudentModel.query.all()
+    return render_template('index.html',students=students)
+
+@app.route('/<int:id>/edit',methods=['GET','POST'])
+
+def update(id):
+    student = StudentModel.query.filter_by(id=id).first()
+
+    if request.method == 'POST':
+           db.session.delete(student)
+           db.session.commit()          
+           if student:
+                hobby = request.form.getlist('hobbies')
+                hobbies =','.join(map(str,hobby))
+                first_name=request.form['first_name']
+                last_name=request.form['last_name']
+                email=request.form['email']
+                password=request.form['password']
+                gender=request.form['gender']
+                hobbies=hobbies
+                country=request.form['country']
+
+                student= StudentModel{
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    password=password,
+                    gender=gender,
+                    hobbies=hobbies,
+                    country=country
+                }
+                db.session.update(student)
+                db.session.commit()
+                return redirect('/')
+            return f'Student with id = {id} Does not exist'
+
+    return render_template('update.html',student=student)
+
+@app.route('/<int:id>/delete',methods=['GET','POST'])
+
+def delete(id):
+    students=StudentModel.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if students:
+            db.session.delete(students)
+            db.session.commit()
+            return redirect('/')
+            abort(404)
+        #return redirect('/')
+        return render_template('delete.html')
+
 
 app.run(host='localhost',port=5000)
