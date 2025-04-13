55e61c66492a259193908e99166a41dd8479cb7e
SPraveeen <praveenji8498@gmail.com>
2025-04-13 15:46:27 +0530

    error adding

--- App.py
+++ App.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
+from models import db,StudentModel
 app=Flask(__name__)
 
-@app.route('/Hello')
-def foo():
-    return 'Welcome to Flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+# @app.before_first_request
+def create_table():
+    db.create_all()
+create_table()
+
+@app.route('/create',methods=['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.html')
+
 app.run(host='localhost',port=5000)
